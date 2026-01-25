5c421cd82c38040b93817d8856b3104fc9db25a0
alaaomran852 <alaaomran852@gmail.com>
2026-01-25 16:09:45 +0200

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -1,17 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("briefForm");
-    
     if (form) {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
-            const name = form.name.value;
-            const project = form.project.value;
-            const service = form.service ? form.service.value : "";
-            const notes = form.notes.value;
-
-            const text = `مرحباً Alaa..%0Aأنا ${name}%0Aمحتاج شغل: ${project}%0Aالخدمة: ${service}%0Aالتفاصيل: ${notes}`;
-            const phone = "201064255553"; 
-            window.open(`https://wa.me/${phone}?text=${text}`, '_blank');
+            const text = `مرحباً..%0Aأنا: ${form.name.value}%0Aمحتاج: ${form.project.value}%0Aالتفاصيل: ${form.notes.value}`;
+            window.open(`https://wa.me/201064255553?text=${text}`, '_blank');
         });
     }
 });
